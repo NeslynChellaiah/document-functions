6cbefee86ab2b4d0ae19f6942c54a10ed0ed173d
Neslyn <neslynchellaiah@gmail.com>
2025-01-12 14:43:15 -0400

    feat: add static docs to function declaration when selected with mouse

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,83 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
+import * as acorn from "acorn";
+import * as walk from 'acorn-walk';
 import * as vscode from 'vscode';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
+const EDITOR: vscode.TextEditor | undefined = vscode.window.activeTextEditor;
+
 export function activate(context: vscode.ExtensionContext) {
+	const listenSelection: vscode.Disposable = vscode.window
+		.onDidChangeTextEditorSelection((event: vscode.TextEditorSelectionChangeEvent) => {
+			let timeout: NodeJS.Timeout | undefined;
+			if (timeout) {
+				clearTimeout(timeout);
+			}
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "document-functions" is now active!');
+			timeout = setTimeout(() => {
+				addDocument(event);
+			}, 100);
+		});
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('document-functions.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Document Functions!');
-	});
-
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(listenSelection);
 }
 
-// This method is called when your extension is deactivated
-export function deactivate() {}
+function addDocument(event: vscode.TextEditorSelectionChangeEvent) {
+	if (event.kind !== 2) {
+		return;
+	}
+	const currentEditor: vscode.TextEditor = event.textEditor;
+	if (currentEditor.selection.isEmpty || !currentEditor.selection.isSingleLine) {
+		return;
+	}
+
+	const code: string = currentEditor.document.getText();
+	try {
+		const ast: acorn.Program = acorn.parse(code, { ecmaVersion: 2020 });
+		walk.simple(ast, {
+			FunctionDeclaration(node: acorn.FunctionDeclaration | acorn.AnonymousFunctionDeclaration) {
+				AddDocsToFnDeclaration(node, currentEditor);
+			},
+		});
+	} catch (error: any) {
+		vscode.window.showErrorMessage(error.message);
+	}
+}
+
+function AddDocsToFnDeclaration(node: acorn.FunctionDeclaration | acorn.AnonymousFunctionDeclaration, currentEditor: vscode.TextEditor) {
+	if (!node?.id?.start && !node?.id?.end) {
+		return;
+	}
+	const startPos: vscode.Position | undefined = EDITOR?.document.positionAt(node.id?.start);
+	const endPos: vscode.Position | undefined = EDITOR?.document.positionAt(node.id?.end);
+	if (!startPos || !endPos || startPos.isEqual(endPos)) {
+		return;
+	}
+	
+	const currentSelection: vscode.Selection = currentEditor.selection;
+	if (cannotAddDocs(startPos, endPos, currentSelection)) {
+		return;
+	}
+
+	insertDocs(startPos);
+}
+
+function cannotAddDocs(startPos: vscode.Position, endPos: vscode.Position, currentSelection: vscode.Selection) {
+	const range: vscode.Range = new vscode.Range(startPos, endPos);
+
+	return (!startPos.isEqual(currentSelection.start) || !endPos.isEqual(currentSelection.end) 
+		|| !startPos.isEqual(range.start) || !endPos.isEqual(range.end));
+}
+
+function insertDocs(startPos: vscode.Position) {
+	const insertPosition = new vscode.Position(startPos.line, 0);
+	EDITOR?.edit(editBuilder => {
+		editBuilder.insert(insertPosition, "/**Static Docs\n**/\n");
+		}).then(success => {
+			if (!success || !EDITOR) {
+				return;
+			}
+			const newSelection = new vscode.Selection( EDITOR?.selection.end,  EDITOR?.selection.end);
+			EDITOR.selection = newSelection;
+		});
+}
+
+export function deactivate() { }			
